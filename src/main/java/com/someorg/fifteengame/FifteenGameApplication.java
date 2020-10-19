80c9b81d8bd4e3a1344d93cbbd50530cef981a5e
Rimas <rimas.kalpokas@gmail.com>
2020-10-19 21:53:04 +0300

    First commit of application

---
+++ src/main/java/com/someorg/fifteengame/FifteenGameApplication.java
@@ -0,0 +1,13 @@
+package com.someorg.fifteengame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifteenGameApplication.class, args);
+    }
+
+}
